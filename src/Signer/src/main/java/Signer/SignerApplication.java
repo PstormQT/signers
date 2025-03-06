50c36de2e9008ed6e0bc9f23848f39e3d3699684
PstormQT <131224632+PstormQT@users.noreply.github.com>
2025-03-06 11:45:36 -0500

    Init all maven files for postgres connection

---
+++ src/Signer/src/main/java/Signer/SignerApplication.java
@@ -0,0 +1,13 @@
+package Signer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignerApplication.class, args);
+	}
+
+}
