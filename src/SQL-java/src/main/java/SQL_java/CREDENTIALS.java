26776dc342b2e3496c36148c47ae923bf0a9d872
Brandon Yi <bjy8512@g.rit.edu>
2025-03-18 18:13:20 -0400

    Updated CREDENTIALS

---
+++ src/SQL-java/src/main/java/SQL_java/CREDENTIALS.java
@@ -0,0 +1,14 @@
+package SQL_java;
+/**
+ 
+Please before asking why the connection is not establish, EDIT THIS PAGE
+to reflect the current password. I will add this file in .gitignore after first init so don't worry
+Also REMEMBER to rename THIS FILE to abc.java so that the .gitignore actualy works
+*/
+
+
+public record abc() {
+    public static final String DBLink = "jdbc:postgresql://127.0.0.1:5432/p32001_05";
+    public static final String USERNAME = "ptd7904";
+    public static final String PASSWORD = "A";
+}
